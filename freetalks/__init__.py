1c627347a55faadc28cd975d313ec45a84fcba21
Silas Sewell <silas@sewell.ch>
2010-11-03 18:49:06 -0400

    Make generic more liberal although it doesn't match all valid keys

--- freetalks/__init__.py
+++ freetalks/__init__.py
@@ -3,5 +3,5 @@ from freetalks import handler
 
 application = webapp2.WSGIApplication([
     webapp2.Route(r'/', handler.general.Home, 'home'),
-    webapp2.Route(r'/talk/<talk:[a-z\d-]*>', handler.talk.Display, 'talk-display'),
+    webapp2.Route(r'/talk/<talk:[\w]+>', handler.talk.Display, 'talk-display'),
 ])
