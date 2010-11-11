622ba27c14f0217b62c4772529addcd23d6cc094
Philip Reichenberger <preichen@gmail.com>
2010-11-10 23:21:49 -0500

    Added series model test

---
+++ tests/test_series.py
@@ -0,0 +1,21 @@
+from fixtures import dataset
+from freetalks.models import Series
+from google.appengine.api.users import User
+import unittest
+
+class TestSeries(unittest.TestCase):
+    def setUp(self):
+        dataset.setup()
+        self._test_user = User(email='test@freetalks.com')
+
+    def tearDown(self):
+        dataset.clear()
+
+    def test_not_saved_if_name_is_empty(self):
+        series = Series(name='',
+                        slug='slug',
+                        link='http://test',
+                        created_user=self._test_user,
+                        updated_user=self._test_user)
+        series.put()
+        self.assertFalse(series.is_saved())
