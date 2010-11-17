bb0a0953f2e3041eb8add35024b07182a65aedf6
Silas Sewell <silas@sewell.ch>
2010-11-17 18:19:35 -0500

    Various style tweaks

--- tests/test_series.py
+++ tests/test_series.py
@@ -4,6 +4,7 @@ from google.appengine.api.users import User
 import unittest
 
 class TestSeries(unittest.TestCase):
+
     def setUp(self):
         dataset.setup()
         self._test_user = User(email='test@freetalks.com')
@@ -12,10 +13,12 @@ class TestSeries(unittest.TestCase):
         dataset.clear()
 
     def test_not_saved_if_name_is_empty(self):
-        series = Series(name='',
-                        slug='slug',
-                        link='http://test',
-                        created_user=self._test_user,
-                        updated_user=self._test_user)
+        series = Series(
+            name='',
+            slug='slug',
+            link='http://test',
+            created_user=self._test_user,
+            updated_user=self._test_user,
+        )
         series.put()
         self.assertFalse(series.is_saved())
