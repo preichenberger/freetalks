7294e87a39309a00337b8c719e001d643e88cc19
Philip Reichenberger <preichen@gmail.com>
2010-11-17 18:21:43 -0500

    Removed test

--- tests/test_series.py
+++ tests/test_series.py
@@ -10,12 +10,3 @@ class TestSeries(unittest.TestCase):
 
     def tearDown(self):
         dataset.clear()
-
-    def test_not_saved_if_name_is_empty(self):
-        series = Series(name='',
-                        slug='slug',
-                        link='http://test',
-                        created_user=self._test_user,
-                        updated_user=self._test_user)
-        series.put()
-        self.assertFalse(series.is_saved())
