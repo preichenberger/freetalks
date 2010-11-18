8cea14944c01ef7d134720aaf29ee1634f638a62
Silas Sewell <silas@sewell.ch>
2010-11-17 20:17:14 -0500

    Add basic talk index

--- freetalks/__init__.py
+++ freetalks/__init__.py
@@ -3,5 +3,6 @@ from freetalks import handler
 
 application = webapp2.WSGIApplication([
     webapp2.Route(r'/', handler.general.Home, 'home'),
+    webapp2.Route(r'/talks', handler.talk.List, 'talk-list'),
     webapp2.Route(r'/talks/<id:[\d]+>', handler.talk.Display, 'talk-display'),
 ])
