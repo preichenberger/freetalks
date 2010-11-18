5227a5b2355db56258ba50b59525ea4285a4b09f
Philip Reichenberger <preichen@gmail.com>
2010-11-17 20:59:12 -0500

    Added series_talks to series handler, updated series template

--- freetalks/handler/series.py
+++ freetalks/handler/series.py
@@ -13,9 +13,13 @@ class Display(web.Handler):
     def get(self, slug):
         series_query = models.Series.all()
         series_query.filter("slug =", slug)
-        series = series_query.fetch(limit=1)
-
-        if series is not None:
-            self.render('series/display.html', content='Render series: %s' % series)
-        else:
+        series = series_query.get()
+       
+        if series is None:
             self.set_404()
+
+        talks_query = models.Talk.all()
+        talks_query.filter('series =', series.key())
+        series_talks = talks_query.fetch(limit=10)
+
+        self.render('series/display.html', series=series, series_talks=series_talks)
